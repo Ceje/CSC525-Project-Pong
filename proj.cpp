0cfca248d889cd73a3c1fc56b42986f6cee4837f
Jake Allen <jakedallen@me.com>
2013-10-18 01:51:50 -0500

    Test base
    Added a very basic test program to show a proof of concept on the pong
    implementation. Allows for independent movement of two player paddles.

---
+++ proj.cpp
@@ -0,0 +1,191 @@
+/*========================================================================
+ *Programmer:	    Jacob Allen
+ *Network Folder:   jacob03
+ *Course:	    CSC 525
+ *Assignment:	    HW2
+ *Modified By:	    N/A
+ *Creation Date:    9/24/2013
+ *Mod Date:	    9/27/2013
+ *Description:	    A program to fill polygons defined from files.
+ *Note:		    N/A
+ *Files:	    hw2.cpp, Makefile, hwproj.sln
+ *IDE/Compiler:	    VIm, GNU g++ / Microsoft Visual Studio 2012
+ *Instructions for Compilation and Execution:
+ *  On posix compliant machine with the GNU C-Compiler and Make installed:
+ *	1.  change to the directory with source files using cd command.
+ *	2.  run 'make'   to BUILD (Compile and Link)
+ *	3.  type './hw2' to EXECUTE
+ *  Visual Studio 2012
+ *	1.  Double click on hwproj.sln to open the project.
+ *	2.  Press Ctrl+F7   to COMPILE
+ *	3.  Press Ctrl+Shift+B  to BUILD (Compile and Link)
+ *	4.  Press Ctrl+F5   to EXECUTE
+ * =====================================================================*/
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void drawScene();
+
+using namespace std;
+
+int p1 = 25;
+int p2 = 25;
+bool wup = true;
+bool sup = true;
+bool auup = true;
+bool adup = true;
+
+void paddleMove()
+{
+    if(!wup)
+    {
+	p1+=1;
+    }
+    else if(!sup)
+    {
+	p1-=1;
+    }
+
+    if(!auup)
+    {
+	p2+=1;
+    }
+    else if(!adup)
+    {
+	p2-=1;
+    }
+}
+
+// OpenGL functions *******************************************************
+void timerCallback(int value)
+{
+    paddleMove();
+    drawScene();
+    glutTimerFunc(3, timerCallback, 0);
+}
+
+void handleKeyUp(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+	case 'w':
+	    {
+		wup = true;
+		//cout << "w up" << endl;
+		break;
+	    }
+	case 's':
+	    {
+		sup = true;
+		break;
+	    }
+	case 'o':	//up arrow
+	    {
+		auup = true;
+		break;
+	    }
+	case 'l':
+	    {
+		adup = true;
+		break;
+	    }
+    }
+}
+
+void handleKeypress(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+	case 27: //Escape Key
+	    {
+		//tearDown();
+		exit(0); //Exit the program
+	    }
+	case 'w':
+	    {
+		wup = false;
+		break;
+	    }
+	case 's':
+	    {
+		sup = false;
+		break;
+	    }
+	case 'o':
+	    {
+		auup = false;
+		break;
+	    }
+	case 'l':
+	    {
+		adup =false;
+		break;
+	    }
+    }
+    //drawScene();
+}
+
+void initRendering()
+{
+    glClearColor(1, 1, 1, 0);		//Specify Background Color: white
+    gluOrtho2D(-200, 200, -200, 200);	//Define the boundries of the viewing area
+    glutIgnoreKeyRepeat(1);		//Tells glut to ignore key repeat from holding down a key.
+    //glEnable(GL_DEPTH_TEST);
+}
+
+void drawScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(1);
+    glLineWidth(1);
+    // Draw the coordinate Axis;
+    glColor3f(0, 0, 0);
+    glBegin(GL_QUADS);			//Draw the x, y coordinate axies.
+	glVertex2i(-170, p1);
+	glVertex2i(-170, p1-50);
+	glVertex2i(-190, p1-50);
+	glVertex2i(-190, p1);
+
+	glVertex2i(170, p2-50);
+	glVertex2i(170, p2);
+	glVertex2i(190, p2);
+	glVertex2i(190, p2-50);
+    glEnd();
+
+    glFlush();
+}
+
+// The Main function of the program***********************************************************
+int main(int argc, char** argv)
+{
+      // OpenGL Startup ****************************************
+    glutInit(&argc, argv);
+    glutInitWindowSize(400, 400);
+
+    glutCreateWindow("Proj Test");
+    initRendering();
+
+    glutDisplayFunc(drawScene);
+    glutKeyboardFunc(handleKeypress);
+    glutKeyboardUpFunc(handleKeyUp);
+
+    glutTimerFunc(3, timerCallback, 0);
+
+    glutMainLoop();
+    // End OpenGL ********************************************
+
+    return 0;
+}
+
