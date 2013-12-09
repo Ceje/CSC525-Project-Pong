28215d212947c8b98f7e1b6a81ff3756c850f0a0
CJ <done245@live.missouristate.edu>
2013-12-09 01:32:40 -0600

    missed a file

--- DesignatedProjectProject/DesignatedProjectProject/menus.cpp
+++ DesignatedProjectProject/DesignatedProjectProject/menus.cpp
@@ -19,50 +19,7 @@ void gameMenu(int i){
 	gameWindow(i);
 }
 void playerColorMenu(int i){
-	int winID=glutGetWindow();
-	float color[3];
-	switch(i){
-	case 0:
-		color[0]=0;
-		color[1]=0;
-		color[2]=0;
-		break;
-	case 1:
-		color[0]=1;
-		color[1]=1;
-		color[2]=1;
-		break;
-	case 2:
-		color[0]=1;
-		color[1]=0;
-		color[2]=0;
-		break;
-	case 3:
-		color[0]=0;
-		color[1]=1;
-		color[2]=0;
-		break;
-	case 4:
-		color[0]=0;
-		color[1]=0;
-		color[2]=1;
-		break;
-	case 5:
-		//getCustomColor
-		break;
-	}
-	if(winID=windows[7]){
-		//playerColor[0]=color;
-	}
-	if(winID=windows[8]){
-		//playerColor[1]=color;
-	}
-	if(winID=windows[9]){
-		//playerColor[2]=color;
-	}
-	if(winID=windows[10]){
-		//playerColor[3]=color;
-	}
+
 }
 
 void exitMenu(int i){
